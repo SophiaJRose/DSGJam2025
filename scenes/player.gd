dc48bbbb3244438e4feef2fc72d7acd80acfc3de
Sophia Jennifer Rose <sophia.j.rose@proton.me>
2025-03-01 17:30:00 +0000

    Added platforms, tweaked rotation

--- scenes/player.gd
+++ scenes/player.gd
@@ -17,9 +17,6 @@ const copy_offsets = [256, -256]
 @onready var left_action = "Left P%s" % player_slot
 @onready var right_action = "Right P%s" % player_slot
 
-func _ready() -> void:
-	pass
-
 func _physics_process(delta: float) -> void:
 	# Handle jump and gravity
 	if Input.is_action_just_pressed(jump_action) and is_on_floor():
@@ -30,22 +27,18 @@ func _physics_process(delta: float) -> void:
 		velocity.y -= (gravity / 2) * delta
 	else:
 		velocity.y -= (gravity) * delta
-
 	var direction := Input.get_axis(left_action, right_action)
 	if direction:
 		velocity.x = direction * SPEED
 		animations.scale.x = -sign(direction)
-		#print(("Walk P%s Vuln" if vulnerable else "Walk P%s") % player_slot)
 		animations.play(("Walk P%s Vuln" if vulnerable else "Walk P%s") % player_slot)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animations.play(("Stand P%s Vuln" if vulnerable else "Stand P%s") % player_slot)
-
 	move_and_slide()
 	copy.global_position = Vector3(position.x + copy_offsets[player_slot-1], position.y, position.z)
 	copyAnimations.play(animations.animation)
 	copyAnimations.scale.x = animations.scale.x
-
 
 func _on_world_rotation_area_entered(area):
 	vulnerable = true
--- scenes/rotating_splitscreen.gd
+++ scenes/rotating_splitscreen.gd
@@ -12,7 +12,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotation += 1.0 * delta
+	rotation += 0.75 * delta
 	topCam.rotation.z = -rotation
 	bottomCam.rotation.z = -rotation
 	worldRotationAreaA.rotation.z = -rotation
