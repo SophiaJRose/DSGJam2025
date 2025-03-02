46a5cb698931e98e0319dac56bf8e795c6dd4d91
Sophia Jennifer Rose <sophia.j.rose@proton.me>
2025-03-02 00:49:47 +0000

    Added sword swing

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,20 +4,64 @@ extends CharacterBody3D
 @export var device_id: int = -1
 
 var vulnerable: bool = false
+var direction: float = 1.0
+var sword_active: bool = false
+var sword_end_timer: int = 0
 
 const SPEED = 32.0
 const JUMP_VELOCITY = 48.0
 const gravity = 112.0
 const copy_offsets = [256, -256]
+const sword_start_angle = deg_to_rad(60)
+const sword_end_angle = deg_to_rad(-45)
+const sword_end_hold = 20
 
 @onready var animations = $AnimatedSprite3D
 @onready var copy = $Copy
 @onready var copyAnimations = $Copy/AnimatedSprite3D
+@onready var sword = $Sword
+@onready var swordSprite = $Sword/SwordArea/SwordSprite
+@onready var swordCopy = $Copy/SwordCopy
+@onready var swordCopySprite = $Copy/SwordCopy/SwordArea/SwordSprite
 @onready var jump_action = "Jump P%s" % player_slot
 @onready var left_action = "Left P%s" % player_slot
 @onready var right_action = "Right P%s" % player_slot
 
+func _ready():
+	swordSprite.play("P%s" % player_slot)
+	swordCopySprite.play("P%s" % player_slot)
+
+func _unhandled_input(event):
+	if event.is_action_pressed("Melee P%s" % player_slot):
+		## Start sword swing
+		sword_active = true
+		sword.visible = true
+		swordCopy.visible = true
+		sword_end_timer = 0
+		if animations.scale.x == 1:
+			sword.rotation.y = deg_to_rad(180)
+			swordCopy.rotation.y = deg_to_rad(180)
+		else:
+			sword.rotation.y = 0
+			swordCopy.rotation.y = 0
+		sword.rotation.z = sword_start_angle
+		swordCopy.rotation.z = sword_start_angle
+	elif event.is_action_pressed("Mine P%s" % player_slot):
+		## do lay mine
+		pass
+
 func _physics_process(delta: float) -> void:
+	# Handle sword movement
+	if sword.rotation.z > sword_end_angle:
+		sword.rotation.z -= deg_to_rad(10.5)
+		swordCopy.rotation.z -= deg_to_rad(10.5)
+	else:
+		sword_end_timer += 1
+		if (sword_end_timer >= sword_end_hold):
+			sword_active = false
+			sword.visible = false
+			swordCopy.visible = false
+			sword_end_timer = 0
 	# Handle jump and gravity
 	if Input.is_action_just_pressed(jump_action) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -27,7 +71,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y -= (gravity / 2) * delta
 	else:
 		velocity.y -= (gravity) * delta
-	var direction := Input.get_axis(left_action, right_action)
+	direction = Input.get_axis(left_action, right_action)
 	if direction:
 		velocity.x = direction * SPEED
 		animations.scale.x = -sign(direction)
--- scenes/rotating_splitscreen.gd
+++ scenes/rotating_splitscreen.gd
@@ -5,6 +5,12 @@ extends Control
 @onready var worldRotationAreaA: Area3D = $BottomScreenContainer/WorldRotationAreaA
 @onready var worldRotationAreaB: Area3D = $TopScreenContainer/WorldRotationAreaB
 
+const max_rotation = 0.75
+const rotation_accel_threshold = 0.15
+const rotation_thresholds = [0.15, 0.45, 0.75]
+const rotation_accels = [0.001, 0.005, 0.01]
+var rotation_speed = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -12,7 +18,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotation += 0.75 * delta
+	for i in rotation_thresholds.size():
+		if rotation_speed < rotation_thresholds[i]:
+			rotation_speed += rotation_accels[i]
+			break
+	rotation += rotation_speed * delta
 	topCam.rotation.z = -rotation
 	bottomCam.rotation.z = -rotation
 	worldRotationAreaA.rotation.z = -rotation
